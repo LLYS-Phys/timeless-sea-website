b1f1709d5f688d22c8707850470eb8c08b9385ab
LLYS-Phys <lachyordanov@gmail.com>
2024-09-20 16:36:11 +0300

    add basic pages and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        children: [
+            {
+                path: 'home',
+                component: HomeComponent
+            }
+        ]
+    },
+    {
+        path: 'contacts',
+        loadComponent: () => import('./contacts/contacts.component').then((comp) => comp.ContactsComponent)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./about/about.component').then((comp) => comp.AboutComponent)
+    },
+    {
+        path: 'gallery',
+        loadComponent: () => import('./gallery/gallery.component').then((comp) => comp.GalleryComponent)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./not-found/not-found.component').then((comp) => comp.NotFoundComponent)
+    }
+];
