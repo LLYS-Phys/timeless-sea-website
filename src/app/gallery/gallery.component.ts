7a7287e7bd0ae30ed88210321d04706374249438
LLYS-Phys <lachyordanov@gmail.com>
2024-09-22 10:36:26 +0300

    add basics for gallery

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GalleryModule, GalleryItem, ImageItem } from 'ng-gallery';
+import { GalleryService } from './gallery.service';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [GalleryModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss'
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
+  interiorImages: GalleryItem[] = []
+  blueRoomImages: GalleryItem[] = []
+  greenRoomImages: GalleryItem[] = []
+  redRoomImages: GalleryItem[] = []
+  outImages: GalleryItem[] = []
+  gardenImages: GalleryItem[] = []
 
+  constructor(private galleryService: GalleryService) {}
+
+  ngOnInit() {
+    this.interiorImages = this.galleryService.getHouseImages()
+    this.blueRoomImages = this.galleryService.getBlueRoomImages()
+    this.greenRoomImages = this.galleryService.getGreenRoomImages()
+    this.redRoomImages = this.galleryService.getRedRoomImages()
+    this.outImages = this.galleryService.getOutImages()
+    this.gardenImages = this.galleryService.getGardenImages()
+  }
 }
---
+++ src/app/gallery/gallery.data.ts
@@ -0,0 +1,64 @@
+export const listHouseImages = () => {
+    return [
+        {name: 'Bathroom.jpg', alt: 'Bathroom'},
+        {name: 'Bathroom2.jpg', alt: 'Bathroom'},
+        {name: 'Bathroom4.jpg', alt: 'Bathroom'},
+        {name: 'Hall.jpg', alt: 'Hall'},
+        {name: 'Hall2.jpg', alt: 'Hall'},
+        {name: 'Kitchen.png', alt: 'Kitchen'},
+        {name: 'Kitchen2.jpg', alt: 'Kitchen'},
+        {name: 'Kitchen4.jpg', alt: 'Kitchen'},
+        {name: 'Kitchen5.jpg', alt: 'Kitchen'}
+    ]
+}
+
+export const listBlueRoomImages = () => {
+    return [
+        {name: 'BlueRoom.jpg', alt: 'Bedroom N.1'},
+        {name: 'BlueRoom1.jpg', alt: 'Bedroom N.1'},
+        {name: 'BlueRoom2.jpg', alt: 'Bedroom N.1'},
+        {name: 'BlueRoom3.jpg', alt: 'Bedroom N.1'}
+    ]
+}
+
+export const listRedRoomImages = () => {
+    return [
+        {name: 'RedRoom4.jpg', alt: 'Bedroom N.2'},
+        {name: 'RedRoom5.jpeg', alt: 'Bedroom N.2'},
+        {name: 'RedRoom6.jpeg', alt: 'Bedroom N.2'}
+    ]
+}
+
+export const listGreenRoomImages = () => {
+    return [
+        {name: 'Green Room .jpg', alt: 'Bedroom N.3'},
+        {name: 'GreenRoom1.jpg', alt: 'Bedroom N.3'},
+        {name: 'GreenRoom3.jpg', alt: 'Bedroom N.3'},
+        {name: 'GreenRoom4.jpg', alt: 'Bedroom N.3'},
+        {name: 'GreenRoom5.jpeg', alt: 'Bedroom N.3'}
+    ]
+}
+
+export const listOutImages = () => {
+    return [
+        {name: 'Out7.png', alt: 'Outside of the house'},
+        {name: 'Out10.png', alt: 'Outside of the house'},
+        {name: 'Out12.jpeg', alt: 'Outside of the house'},
+        {name: 'Out13.jpeg', alt: 'Outside of the house'},
+        {name: 'Out14.jpeg', alt: 'Outside of the house'},
+        {name: 'Out15.jpeg', alt: 'Outside of the house'}
+    ]
+}
+
+export const listGardenImages = () => {
+    return [
+        {name: 'Jacuzzi.png', alt: 'Jacuzzi'},
+        {name: 'Jacuzzi1.png', alt: 'Jacuzzi'},
+        {name: 'Out1.jpg', alt: 'Garden'},
+        {name: 'Out3.jpg', alt: 'Garden'},
+        {name: 'Out4.jpg', alt: 'Garden'},
+        {name: 'Out11.jpg', alt: 'Garden'},
+        {name: 'Out16.jpeg', alt: 'Garden'},
+        {name: 'Out17.jpg', alt: 'Garden'}
+    ]
+}
