3548b9ee700442020a6ebfc162a2f34a0f83b3bd
LLYS-Phys <lachyordanov@gmail.com>
2024-09-23 01:12:11 +0300

    optimize gallery service and connect firebase database

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnInit } from '@angular/core';
 import { GalleryModule, GalleryItem, ImageItem } from 'ng-gallery';
 import { GalleryService } from './gallery.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-gallery',
@@ -10,21 +12,47 @@ import { GalleryService } from './gallery.service';
   styleUrl: './gallery.component.scss'
 })
 export class GalleryComponent implements OnInit {
-  interiorImages: GalleryItem[] = []
-  blueRoomImages: GalleryItem[] = []
-  greenRoomImages: GalleryItem[] = []
-  redRoomImages: GalleryItem[] = []
-  outImages: GalleryItem[] = []
-  gardenImages: GalleryItem[] = []
+  images: {
+    interiorImages: GalleryItem[],
+    blueRoomImages: GalleryItem[],
+    greenRoomImages: GalleryItem[],
+    redRoomImages: GalleryItem[],
+    outImages: GalleryItem[],
+    gardenImages: GalleryItem[] 
+  } = { interiorImages: [], blueRoomImages: [], greenRoomImages: [], redRoomImages: [], outImages: [], gardenImages: [] }
+  allImages: string[] = []
 
-  constructor(private galleryService: GalleryService) {}
+  constructor(private galleryService: GalleryService, private http: HttpClient, private destroRef: DestroyRef) {}
 
   ngOnInit() {
-    this.interiorImages = this.galleryService.getImages('house')
-    this.blueRoomImages = this.galleryService.getImages('blue')
-    this.greenRoomImages = this.galleryService.getImages('green')
-    this.redRoomImages = this.galleryService.getImages('red')
-    this.outImages = this.galleryService.getImages('out')
-    this.gardenImages = this.galleryService.getImages('garden')
+    const imagesSubscription = this.http
+                          .get<Observable<any>>('https://timeless-sea-default-rtdb.europe-west1.firebasedatabase.app/images.json')
+                          .subscribe({
+                            next: (data) => {
+                              data.forEach((el) => {
+                                this.allImages.push(el
+                                                      .replaceAll("'", "\"")
+                                                      .replace("name", "\"name\"")
+                                                      .replace("alt", "\"alt\"")
+                                                      .replace("type", "\"type\"")
+                                )
+                              })
+                            },
+                            complete: () => {
+                              this.images = {
+                                interiorImages: this.galleryService.getImages('house', this.allImages),
+                                blueRoomImages: this.galleryService.getImages('blue', this.allImages),
+                                greenRoomImages: this.galleryService.getImages('green', this.allImages),
+                                redRoomImages: this.galleryService.getImages('red', this.allImages),
+                                outImages: this.galleryService.getImages('out', this.allImages),
+                                gardenImages: this.galleryService.getImages('garden', this.allImages)
+                              }
+                            },
+                            error: (err) => {
+                              console.log(err)
+                            }
+                          })
+                          
+    this.destroRef.onDestroy(() => imagesSubscription.unsubscribe())
   }
 }
--- src/app/gallery/gallery.service.ts
+++ src/app/gallery/gallery.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@angular/core";
 import { ImageItem } from "ng-gallery";
-import { listImages } from "./gallery.data";
 
 @Injectable({providedIn: 'root'})
 export class GalleryService {
-    getImages(type: string) {
+    getImages( type: 'house' | 'blue' | 'red' | 'green' | 'out' | 'garden', allImages: string[] ) {
         let images: ImageItem[] = []
-        listImages(type).forEach((image) => {
-            images.push(new ImageItem({src: '/images/' + image.name, thumb: '/images/' + image.name, alt: image.alt}))
+        allImages
+            .filter((image) => JSON.parse(image).type === type)
+            .forEach((image) => {
+                images.push(new ImageItem({src: '/images/' + JSON.parse(image).name, thumb: '/images/' + JSON.parse(image).name, alt: JSON.parse(image).alt}))
         })
         return images
     }
