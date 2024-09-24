3701d0e318f2df992b4db34ac3cee4bc7b4d16fc
LLYS-Phys <lachyordanov@gmail.com>
2024-09-24 16:34:00 +0300

    add basics for email service with emailjs

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contacts',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './contacts.component.scss'
 })
 export class ContactsComponent {
+  public sendEmail(e: Event) {
+    e.preventDefault();
 
+    emailjs
+      .sendForm('service_zoo53hi', 'template_usamvtl', e.target as HTMLFormElement, {
+        publicKey: 'LPAjwHkkePZIBgOue',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error: any) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 }
