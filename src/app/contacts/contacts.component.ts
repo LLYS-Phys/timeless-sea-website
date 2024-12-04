7ffe8cc115844bc96a2e8c36067dafd33f2bc124
LLYS-Phys <lachyordanov@gmail.com>
2024-12-04 21:15:43 +0200

    style contact form and connect validators

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -2,11 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Component, DestroyRef } from '@angular/core';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 import { EmailJsType } from './contacts.model';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatInputModule, MatFormFieldModule, ReactiveFormsModule],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
@@ -14,6 +19,18 @@ export class ContactsComponent {
   constructor(private http: HttpClient, private destroyRef: DestroyRef){}
 
   credentials: EmailJsType = {public_key: '', template_id: '', service_id: ''}
+
+  email_sent: boolean = false
+
+  emailForm = new FormGroup({
+    name: new FormControl({value: '', disabled: this.email_sent}, [Validators.required]),
+    email: new FormControl({value: '', disabled: this.email_sent}, [Validators.required, Validators.email]),
+    message: new FormControl({value: '', disabled: this.email_sent}, [Validators.required])
+  })
+
+  get buttonStatus() {
+    return this.emailForm.status == "VALID" ? false : true
+  }
 
   private fetchEmailjsCredentials () {
     return this.http.get<EmailJsType>('https://timeless-sea-default-rtdb.europe-west1.firebasedatabase.app/emailjs.json')
@@ -44,6 +61,8 @@ export class ContactsComponent {
       .then(
         () => {
           console.log('SUCCESS!');
+          this.email_sent = true
+          this.emailForm.disable()
         },
         (error: any) => {
           console.log('FAILED...', (error as EmailJSResponseStatus).text);
