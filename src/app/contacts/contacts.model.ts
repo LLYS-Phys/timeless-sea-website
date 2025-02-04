085fae2b188100edaec1e0eaf59a59b3b5ba0ca7
LLYS-Phys <lachyordanov@gmail.com>
2025-02-04 10:20:30 +0200

    proxy server

--- src/app/contacts/contacts.model.ts
+++
@@ -1,6 +0,0 @@
-export interface EmailJsType {
-    public_key: string,
-    template_id: string,
-    service_id: string,
-    confirmation_template_id: string
-}
--- src/app/contacts/prices.model.ts
+++
@@ -1,8 +0,0 @@
-export interface PricesType {
-    strong_summer_weekday: string,
-    strong_summer_weekend: string,
-    weak_summer_weekday: string,
-    weak_summer_weekend: string,
-    winter_weekday: string,
-    winter_weekend: string
-}
